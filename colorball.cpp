c1e6465b47630d876fc13ab990c77363e79ca3fe
dmin0211 <ehdals0211@naver.com>
2020-11-16 00:10:05 +0900

    revise-vector-collorball

--- colorball.cpp
+++ colorball.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(){
-    //정렬이다임마
-    cout<<"정렬이다임마"<<endl;
-    cout<<"pull이 됐나?"<<endl;
+    int ball_number;
+    cin>>ball_number; // 공의 갯수 입력
+    vector<vector <int>> v(ball_number, vector<int> (2,0));
+    for(int temp =0 ; temp<ball_number ; temp++){
+        cin>>v[temp][0]>>v[temp][1];
+    }
+    /*for(int temp =0 ; temp<ball_number ; temp++){
+        cout<<v[temp][0]<<"\t"<<v[temp][1]<<endl;
+    }*/
+    
 }
