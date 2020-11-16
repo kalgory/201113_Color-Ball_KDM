956d7a1113780f8b1ae11d68764fe8efe6e6993c
dmin0211 <ehdals0211@naver.com>
2020-11-17 01:16:14 +0900

    source update

--- colorball.cpp
+++ colorball.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main(){
-    int ball_number;
+    int ball_number, duplexprocessing=0;
     cin>>ball_number; // 공의 갯수 입력
     vector<vector <int>> v(ball_number, vector<int> (2,0));
-    for(int temp =0 ; temp<ball_number ; temp++){
-        cin>>v[temp][0]>>v[temp][1];
+    vector<int> opervec(ball_number, 0);
+    vector<int> color_index(200001,0);
+    for(int temp =0;temp<ball_number;temp++){
+        cin>>v[temp][1]>>v[temp][0];
     }
-    /*for(int temp =0 ; temp<ball_number ; temp++){
-        cout<<v[temp][0]<<"\t"<<v[temp][1]<<endl;
-    }*/
-    cout<<"gitignore 수정"<<endl;
-        
+    sort(v.begin(),v.end());
+    color_index[v[0][1]]=v[0][0];
+    for(int temp=1;temp<ball_number;temp++){
+        opervec[temp]=opervec[temp-1]+v[temp-1][0]-color_index[v[temp][1]]+duplexprocessing;
+        duplexprocessing=color_index[v[temp][1]];
+        color_index[v[temp][1]]+=v[temp][0];
+    }
+    for(int temp =0 ; temp<ball_number ; temp++){
+        cout<<opervec[temp]<<endl;
+    }
+
+    
 }
